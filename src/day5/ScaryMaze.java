b3353cdb7fa54b0a82d84edb04bd26a0e87262d8
Charlie <hallieherbert@yahoo.com>
2016-04-19 09:46:44 -0700

    Revert "Maze Building!"
    This reverts commit 492b275e8ef7ebb458aba04a405d8ce0227f10f9.

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -6,32 +6,27 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener, MouseListener {
+public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	
 	BufferedImage maze;
-	final int frameWidth = 800;
-	final int frameHeight = 600;
-	int mazeNum = 1; 
+	final int frameWidth = 600;
+	final int frameHeight = 400;
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("Maze2.jpg"));
+		maze = ImageIO.read(getClass().getResource("maze.png"));
 		//2. set the mouse pointer to the start of your maze using:
 		//new Robot().mouseMove(xPosition, yPosition)
-		new Robot().mouseMove(10, 425);
-		addMouseMotionListener(this);
-		addMouseListener(this);
+		
 		//3. add a mouse motion listener using:
 		//addMouseMotionListener(this)
 		
@@ -43,24 +38,12 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener, 
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//4. print the mouseColor variable to see what color the mouse is touching
-System.out.println(mouseColor);
+
 		//5. make a variable to hold the background color. 
-int BC = -15641316;
+
 		//6. if the mouse falls off the path (if it is on the background)
-		if (mouseColor == BC) {
-			scare();
-		}
+		
 				// call the scare method
-		if (mouseColor == -1048547) {
-			AudioClip sound2 = JApplet.newAudioClip(getClass().getResource("Cheer.wav"));
-			showScaryImage("Win.jpg");
-			sound2.play();
-		}
-		if (mouseColor == -3228115) {
-			AudioClip sound2 = JApplet.newAudioClip(getClass().getResource("Button.wav"));
-			showScaryImage("Maze.jpg");
-			sound2.play();
-		}
 		
 		//10. if the mouse is on the end color
 				
@@ -71,12 +54,12 @@ int BC = -15641316;
 	private void scare() {
 		System.out.println("BOO!");
 		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
+		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
 		
 		//8. play the scary sound. Hint: type "sound" and then a period.		
-		sound.play();
+		
 		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
-		showScaryImage("GO.jpg");
+
 	}
 
 	private void showScaryImage(String imageName) {
@@ -94,7 +77,7 @@ int BC = -15641316;
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("Charlie's Scary Maze");
+		JFrame frame = new JFrame("June's Scary Maze");
 		frame.add(this);
 		setPreferredSize(new Dimension(frameWidth, frameHeight));
 		frame.pack();
@@ -110,42 +93,6 @@ int BC = -15641316;
 
 	@Override
 	public void mouseDragged(MouseEvent e) {}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		if (mazeNum == 1) {
-			showScaryImage("Maze3.jpg");
-			mazeNum = 2;
-		}
-		else{
-			showScaryImage("Maze2.jpg");
-			mazeNum = 1;
-		}
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
 
